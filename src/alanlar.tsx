e6b21f4bb5015063bbe57b1174c5f89fa943ff31
mehmetcandinc <dincmehmetcann@gmail.com>
2022-09-07 15:22:28 +0300

    new data

---
+++ src/alanlar.tsx
@@ -0,0 +1,18 @@
+export const alanlarr = [
+    {
+      name: "Müzik",
+      id: "muzik"
+    },
+    {
+      name: "Resim",
+      id: "resim"
+    },
+    {
+      name: "Spor",
+      id: "spor"
+    },
+    {
+      name: "Kitap",
+      id: "kitap"
+    },
+  ];
